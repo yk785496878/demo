7416ff09ebe8c797a40f48eeb1b672ad27850b41
YarkYan <Yark_Yan@human-horizons.com>
2024-02-02 16:34:05 +0800

    modify

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <Poco/DateTimeFormatter.h>
+#include <Poco/DateTimeFormat.h>
 
 int main(){
     std::cout << "Hello World" << std::endl;
+
+    Poco::DateTime now;
+    std::string formattedTime = Poco::DateTimeFormatter::format(now, Poco::DateTimeFormat::SORTABLE_FORMAT);
+    std::cout << "Current time: " << formattedTime << std::endl;
     return 0;
 }
